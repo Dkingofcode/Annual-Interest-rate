41fb49d380fa8195196f4709a2396d6346f35ddd
Dkingofcode <doladepo128@gmail.com>
2023-01-18 12:44:07 +0000

    listed steps withe main function

---
+++ app.py
@@ -0,0 +1,7 @@
+# collect the necessary inputs: principal, apr, years
+# calculate the monthly payment
+# show the user
+
+def main():
+    print(" This is a monthly payment loan calculator ")
+    print("")
